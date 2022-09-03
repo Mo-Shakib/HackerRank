d9d731eb9d6b3bd0dbb4990745da746b854049bc
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-09-04 05:21:16 +0600

    v3.0 - Now make a index.html file

--- README_automation.py
+++ README_automation.py
@@ -2,12 +2,13 @@ import os, time, git
 from datetime import datetime
 from git.objects.commit import Commit 
 import urllib.parse
+import markdown
 
 directory = os.path.dirname(os.path.realpath(__file__))
 
 folderTree = {}
 w_folders = ['1. Algorithms', '2. Data Structures','3. Mathematics']
-ignoreFiles = ['.DS_Store', 'README.md']
+ignoreFiles = ['.DS_Store', 'README.md','index.html']
 fileTyp = {'py':'Python','cpp':'C++','c':'C','js':'JavaScript','java':'Java'}
 totalSolved = 0
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -30,7 +31,9 @@ readme_file.write("""
     <img src="https://github.com/Mo-Shakib/HackerRank/actions/workflows/README_automation.yml/badge.svg">
     <img src="https://wakatime.com/badge/user/8e02bfd3-85d8-4d9d-88df-fa983f91ff30/project/b82b047d-1e9b-4267-a6db-5430b5c24ed5.svg">
     <img src="https://img.shields.io/badge/Language-Python-orange.svg">
+</p>
 """)
+
 
 # lastUpdate = datetime.now()
 # lastUpdate = lastUpdate.strftime("%d/%m/%y")
@@ -90,6 +93,15 @@ for f in sorted_folders:
 print('Total:',totalSolved)
 readme_file.close()
 time.sleep(1)
+
+# Writing website for the readme
+print('Writing index.html file')
+markdown.markdownFromFile(
+    input='README.md',
+    output='index.html',
+    encoding='utf8',
+)
+print('Done writing index.html')
 
 # Push to GitHub
 
