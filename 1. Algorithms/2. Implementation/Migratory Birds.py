1ca6031f266705a9a26e36b02ecfe104197bc55c
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-09-14 06:29:11 +0600

    Accepted :+1:

--- 1. Algorithms/2. Implementation/Migratory Birds.py
+++ 1. Algorithms/2. Implementation/Migratory Birds.py
@@ -5,22 +5,19 @@
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
+# Write your code here
 
-def migratoryBirds(arr):
-    # Write your code here
+def migratoryBirds(arr):    
     birds = {}
-    res = 0
     for i in arr:
         if i not in birds:
             birds[i] = [i]
         else:
             birds[i] = birds[i] + [i]
     
-    arr = [*set(arr)]
-    max_ = i
+    arr = sorted([*set(arr)])
+    max_ = arr[0]
     for i in arr:
         if len(birds[i]) > len(birds[max_]):
             max_ = i
     return max_
-
-migratoryBirds([1, 1, 2, 2, 3])
