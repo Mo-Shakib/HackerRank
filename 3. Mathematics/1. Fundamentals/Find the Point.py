00088f55948c2fd8e810768c876d5e3a7825ab05
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-09-16 23:16:35 +0600

    Accepted :+1:

---
+++ 3. Mathematics/1. Fundamentals/Find the Point.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/find-point/problem
+
+#!/bin/python3
+
+import os
+
+# Complete the 'findPoint' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER px
+#  2. INTEGER py
+#  3. INTEGER qx
+#  4. INTEGER qy
+
+# Solutution
+def findPoint(px, py, qx, qy):    
+    return [2*qx-px, 2*qy-py]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    for n_itr in range(n):
+        first_multiple_input = input().rstrip().split()
+
+        px = int(first_multiple_input[0])
+
+        py = int(first_multiple_input[1])
+
+        qx = int(first_multiple_input[2])
+
+        qy = int(first_multiple_input[3])
+
+        result = findPoint(px, py, qx, qy)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
