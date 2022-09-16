ca093978e4c65c685d9aeae22acb3a4e0c4deaf7
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-09-16 21:43:48 +0600

    Accepted :+1:

--- 1. Algorithms/2. Implementation/Day of the Programmer.py
+++ 1. Algorithms/2. Implementation/Day of the Programmer.py
@@ -8,6 +8,18 @@
 
 # Write your code here
 def dayOfProgrammer(year):
-    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-        return f'12.09.{year}'
+    if year > 1917:
+        if year == 1918:
+            return '26.09.1918'
+
+        if (year % 400 == 0):
+            return f'12.09.{year}'
+        
+        elif (year % 4 == 0 and year % 100 != 0):
+            return f'12.09.{year}'
+    
+    elif year < 1917:
+        if year % 4 == 0:
+            return f'12.09.{year}'
+    
     return f'13.09.{year}'
