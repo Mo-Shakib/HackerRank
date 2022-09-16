b56efd8d7756106cbcd711d543aec7b6b9d1c5a8
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-09-16 12:48:10 +0600

    🛠

---
+++ 1. Algorithms/2. Implementation/Day of the Programmer.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/day-of-the-programmer/problem?h_r=next-challenge&h_v=legacy
+
+# Complete the 'dayOfProgrammer' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER year as parameter.
+#
+
+def dayOfProgrammer(year):
+    # Write your code here
