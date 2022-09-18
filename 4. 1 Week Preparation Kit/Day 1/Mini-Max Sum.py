357848311054091fc42e763c4ea372cb46c99e98
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-09-19 05:23:22 +0600

    Accepted :+1:

---
+++ 4. 1 Week Preparation Kit/Day 1/Mini-Max Sum.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one&h_r=next-challenge&h_v=zen
+
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr = sorted(arr)
+    print(f'{sum(arr[:4])} {sum(arr[1:])}')
