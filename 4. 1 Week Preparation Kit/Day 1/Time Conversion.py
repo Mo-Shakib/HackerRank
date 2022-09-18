8411907d81c8b117f51ff3c2ce4baa10fe9b2844
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-09-19 05:25:15 +0600

    Accepted :+1:

---
+++ 4. 1 Week Preparation Kit/Day 1/Time Conversion.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+def timeConversion(s):
+    # Write your code here
+    if s[-2:] == "AM":
+        if s[:2] == "12":
+            return "00" + s[2:-2]
+        else:
+            return s[:-2]
+    else:
+        if s[:2] == "12":
+            return s[:-2]
+        else:
+            return str(int(s[:2]) + 12) + s[2:-2]
